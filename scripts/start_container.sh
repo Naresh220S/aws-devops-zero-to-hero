bb94b44920b889b26c06c3d7facd403474d2c881
Naresh220S <125388385+Naresh220S@users.noreply.github.com>
2024-12-25 17:35:46 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull naresh220/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 naresh220/simple-python-flask-app 
+docker run -d -p 8000:8000 naresh220/simple-python-flask-app 
